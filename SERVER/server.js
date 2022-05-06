4a84fe82dcb890269b129b774e248477db05e21f
DESKTOP-EDOFIHE\eun20 <eunkyung20516@gmail.com>
2022-05-06 18:27:54 +0900

    server에서 DB table 받아오기 => 비동기 처리 완료

--- SERVER/server.js
+++ SERVER/server.js
@@ -74,34 +74,72 @@ app.post('/login', (req, res) => {  // 로그인 처리
     var pw = req.body.postPw;
     console.log("get_id : " + id + " , get_pw : " + pw);
 
-    // var query = "select id from movies_db.users where id= '" + id + "';";
+    // var query = "INSERT INTO food_db.USERINFO(`user_ID`,`user_PASSWORD`)VALUES('" + id + "','" + pw + "')";
 
-    // db.query(query, function (err, row) {
+    // food_db.query(query, function (err, row) {
     //     if (err) {
-    //         console.log('err :' + err);
-    //     } else {
-    //         console.log("id query문 결과 : " + JSON.stringify(row));
-    //         if (row.length === 0 ) {    // 검색 결과가 0이면 등록 x
-    //             console.log("등록되지 않은 id 입니다.");
-    //             res.send({nic_name : undefined, msg:"id_fail"}); // 클라이언트(login.html) 쪽으로 전달
-    //         } else {
-    //             query = "select pw from movies_db.users where id= '" + id + "';";
-    //             db.query(query, function (err, row) {
-    //                 console.log("pw query문 결과 : " + JSON.stringify(row));
-    //                 if (row[0].pw !== pw) {
-    //                     console.log("비교한 pw : " + pw);
-    //                     console.log("잘못된 비밀번호 입니다.");
-    //                     res.send({nic_name : undefined, msg:"pw_fail"});
-    //                 } else {
-    //                     console.log("로그인에 성공하셨습니다.");
-    //                     query = "select nic_name from movies_db.users where id= '" + id + "';";
-    //                     db.query(query, function (err, row) {
-    //                         console.log("nic_name query문 결과 : " + JSON.stringify(row));
-    //                         res.send({nic_name : row[0].nic_name, msg:"success"});
-    //                     })
-    //                 }
-    //             })
-    //         }
+    //         console.log('err : ' + err);
+    //         res.send({result : "fail"});
     //     }
-    // });
+    //     else {
+    //         // res.send({msg : "success"});
+    //         console.log("로그인 성공");
+    //         res.send({result : "success"});
+    //     }
+    // })
+});
+
+app.post('/recommend', (req, res) => {   // 추천 시스템 처리
+
+    console.log("추천 시스템 처리 실행");
+    console.log("req.body : " + JSON.stringify(req.body));
+    
+    var id = req.body.userId;
+    console.log("get_id : " + id);
+
+    var food_data;
+    var rate_data;
+    var table = "FOOD";
+
+    var tableName = ["FOOD", "RATING"];
+
+    tableList(table, function(food_data) {
+        foodPrint(food_data);
+    });
+
+    table = "RATING";
+    tableList(table, function(rate_data) {
+        ratePrint(rate_data);
+    });
+
+    function foodPrint(food_data) {
+        console.log("food_data : " + JSON.stringify(food_data));
+    }
+    function ratePrint(food_data) {
+        console.log("rate_data : " + JSON.stringify(rate_data));
+    }
+    function tableList(table, callback) {
+        var query = "SELECT * FROM food_db." + table + ";";
+        // console.log(query);
+        food_db.query(query, function (err, row) {
+            if (err) {
+                callback(err);
+                console.log('err :' + err);
+            }
+            else {
+                // console.log("food_info_query 결과 : " + JSON.stringify(row));
+                if (table == "FOOD") {
+                    food_data = row;
+                    callback(row);
+                    return row;
+                }
+                else if (table == "RATING"){
+                    rate_data = row;
+                    callback(row);
+                    return row;
+                }
+            }
+        }); 
+    }
+    
 });
